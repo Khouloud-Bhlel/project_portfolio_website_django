f7fac6bb51dcaaf2a61ad79eade34568b9cff25b
khouloud ben hlel <120328825+5oukha@users.noreply.github.com>
2023-05-22 17:04:10 +0100

    views.py

--- authapp/views.py
+++ authapp/views.py
@@ -1,54 +1,51 @@
+from django.contrib.admin import site
 from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
-# Create your views here.
 def signup(request):
-    
-    if request.method=="POST":
-        get_email=request.POST.get('email')
-        get_password=request.POST.get('pass1')
-        get_confirm_password=request.POST.get('pass2')
-        if get_password!=get_confirm_password:
-            messages.info(request,'Password is not matching')
+    if request.method == "POST":
+        get_email = request.POST.get('email')
+        get_password = request.POST.get('pass1')
+        get_confirm_password = request.POST.get('pass2')
+
+        if get_password != get_confirm_password:
+            messages.error(request, 'Passwords do not match')
             return redirect('/auth/signup/')
-        
-        try:
-            if User.objects.get(username=get_email):
-                messages.warning(request,"Email is Taken")
-                return redirect('/auth/signup/')
-        except Exception as identifier:
-            pass
-        myuser=User.objects.create_user(get_email,get_email,get_password)
-        myuser.save()
 
-        myuser= authenticate(username=get_email,password=get_password)
+        if User.objects.filter(username=get_email).exists():
+            messages.warning(request, 'Email is already taken')
+            return redirect('/auth/signup/')
 
-        if myuser is not None:
+        myuser = User.objects.create_user(get_email, get_email, get_password)
+        login(request, myuser)
+        messages.success(request, 'User created and logged in successfully')
+        return redirect('/')
 
-            login(request,myuser)
-            messages.success(request,"User Created & Login Success")
-            return redirect('/')
-
-        
-    return render(request,'signup.html')
+    return render(request, 'signup.html')
 
 def handleLogin(request):
-    if request.method=="POST":
-        get_email=request.POST.get('email')
-        get_password=request.POST.get('pass1')
-        myuser= authenticate(username=get_email,password=get_password)
+    if request.method == "POST":
+        get_email = request.POST.get('email')
+        get_password = request.POST.get('pass1')
+
+        myuser = authenticate(username=get_email, password=get_password)
 
         if myuser is not None:
-            login(request,myuser)
-            messages.success(request,"Login Success")
+            if myuser.is_superuser or myuser.username == 'admin':
+                messages.success(request, 'Admin logged in successfully')
+            else:
+                messages.success(request, 'User logged in successfully')
+            login(request, myuser)
             return redirect('/')
         else:
-            messages.error(request,"Invalid Credentials")
-    return render(request,'login.html')
+            messages.error(request, 'Invalid credentials')
+
+    site_name = site.site_title if site.site_title else 'Django administration'
+    return render(request, 'login.html', {'site_name': site_name})
 
 def handleLogout(request):
     logout(request)
-    messages.success(request,'logout success')
-    return render(request,'login.html')
+    messages.success(request, 'Logged out successfully')
+    return render(request, 'login.html')
